f20ebd9bfed3f4288f6bfb673559542eaf2cc8ce
Hienphan0111 <hienphan@digitalcad.edu.vn>
2023-08-04 18:10:52 +0700

    Add useFetch hook, accomplished Popular section

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,14 +1,53 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  ActivityIndicator,
+} from 'react-native';
+import { useRouter } from 'expo-router';
+import { COLORS, SIZES } from '../../../constants';
+import PopularJobCard from '../../common/cards/popular/PopularJobCard';
+import useFetch from '../../../hooks/useFetch';
 
-import styles from './popularjobs.style'
+import styles from './popularjobs.style';
 
 const Popularjobs = () => {
-  return (
-    <View>
-      <Text>Popularjobs</Text>
-    </View>
-  )
-}
+  const router = useRouter();
 
-export default Popularjobs
+  const { data, error, isLoading, refetch } = useFetch('search', {
+    query: 'React developer',
+    page: 1,
+    num_pages: 1,
+  });
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popular Jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show all</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator size='large' colors={COLORS.primary} />
+        ) : error ? (
+          <Text>Something went wrong</Text>
+        ) : (
+          <FlatList
+            data={data}
+            renderItem={({ item }) => <PopularJobCard item={item} />}
+            keyExtractor={(item) => item?.job_id}
+            contentContainerStyle={{ columnGap: SIZES.medium }}
+            horizontal
+          />
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default Popularjobs;
